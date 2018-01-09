fc39be120c61969ac0f7994c618944affddbd092
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-01-09 09:38:18 +0100

    update to parent-pom 43 and update headers to 2018

--- src/main/java/org/kohsuke/github/GitHub.java
+++ src/main/java/org/kohsuke/github/GitHub.java
@@ -1,6 +1,6 @@
 /*
  * GitHub API for Java
- * Copyright (C) 2009-2017 SonarSource SA
+ * Copyright (C) 2009-2018 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
